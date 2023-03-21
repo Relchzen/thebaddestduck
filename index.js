7865ea285f123ad111a7dec154955d8d2764c63d
NomieC <Fbdarykids@gmail.com>
2023-03-21 12:39:28 +0700

    coba movememt

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function incSleep(){
     mental_health.value += 1;
 }
 
-//ini code maksudnya pas di click play character lgsg masuk ke main page + udah ada gambar player sebagai char terpilih
+//ini code maksudnya pas di click play character lgsg masuk ke main page + udah ada gambar pimg sebagai char terpilih
 //tapi masih belom muncul gamabrnya, function dah bekerja
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
@@ -40,31 +40,74 @@ var img = document.createElement("img");
 img.src = data;
 document.body.appendChild(img);
 
-function charSelect1() {
-    var playerimg = document.createElement("img");
-    playerimg.src = "./asset/duck-player-unscreen.gif";
+/*function charSelect1() {
+    var pimgimg = document.createElement("img");
+    pimgimg.src = "./asset/duck-pimg-unscreen.gif";
     var targetDoc = window.open('index.html');
     targetDoc.addEventListener('load', function () {
-        var player = targetDoc.document.getElementById('player');
-        player.appendChild(playerimg);
+        var pimg = targetDoc.document.getElementById('pimg');
+        pimg.appendChild(pimgimg);
     });
 
 }
 function charSelect2() {
-    var playerimg = document.createElement("img");
-    playerimg.src = "./asset/duck-player-unscreen.gif";
+    var pimgimg = document.createElement("img");
+    pimgimg.src = "./asset/duck-pimg-unscreen.gif";
     var targetDoc = window.open('index.html');
     targetDoc.addEventListener('load', function () {
-        var player = targetDoc.document.getElementById('player');
-        player.appendChild(playerimg);
+        var pimg = targetDoc.document.getElementById('pimg');
+        pimg.appendChild(pimgimg);
     });
 }
 function charSelect3() {
-    var playerimg = document.createElement("img");
-    playerimg.setAttribute("src","./asset/Slime.play.gif");
+    var pimgimg = document.createElement("img");
+    pimgimg.setAttribute("src","./asset/Slime.play.gif");
     var targetDoc = window.location.href = "index.html";
     targetDoc.addEventListener('load', function () {
-        var player = targetDoc.document.getElementById('player');
-        player.appendChild(playerimg);
+        var pimg = targetDoc.document.getElementById('pimg');
+        pimg.appendChild(pimgimg);
     });
-}
+}*/
+
+var playground = document.getElementById("sangkar");
+var pimg = document.getElementById("pimg");
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+      ctx = playGround.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+  }
+  
+  function updateGameArea(pimg) {
+    playGround.clear();
+    pimg.newPos();
+    pimg.update();
+  }
+  
+  function moveup() {
+    pimg.speedY -= 1;
+  }
+  
+  function movedown() {
+    pimg.speedY += 1;
+  }
+  
+  function moveleft() {
+    pimg.speedX -= 1;
+  }
+  
+  function moveright() {
+    pimg.speedX += 1;
+  }
