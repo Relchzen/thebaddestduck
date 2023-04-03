d2d30b1a23b27922cc7d459379618ed017c3c35a
NomieC <Fbdarykids@gmail.com>
2023-04-03 14:24:02 +0700

    a bit change on music and play time

--- clockScript.js
+++ clockScript.js
@@ -3,23 +3,21 @@ function displayTime(){
     var min = dateTime.getMinutes();
     var sec = dateTime.getSeconds();
     var session = document.getElementById('session');
-
-    if(min >= 12){
-        session.innerHTML = 'PM';
-    }else{
-        session.innerHTML = 'AM';
-    }
     
-
     if(min >= 12&&min<=23){
         min = min - 12;
+        session.innerHTML = "PM"
     }else if(min>=24&&min<=35){
         min = min -24;
+        session.innerHTML = "AM"
     }else if(min>=36&&min<=47){
         min = min - 36;
+        session.innerHTML = "PM"
     }else if(min>=48&&min<=59){
         min = min - 48;
+        session.innerHTML = "AM"
     }
+
 
     document.getElementById('minutes').innerHTML = min;
     document.getElementById('seconds').innerHTML = sec;
