f91e7765ceb60c4d9f9d6b6dc0d6070b26aed60c
Farhan0902 <farhanammarhakim28@gmail.com>
2023-04-03 08:12:13 +0700

    perubahan

--- play.js
+++ play.js
@@ -1,22 +1,101 @@
-// Mendapatkan elemen progress bar HTML
-const progressBar = document.querySelector('#health');
+// board
+var blocksize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
 
-// Memeriksa apakah ada checkpoint progress di local storage
-const progressCheckpoint = localStorage.getItem('progressCheckpoint');
-if (progressCheckpoint) {
-  // Jika ada, mengatur nilai progress bar HTML ke nilai checkpoint
-  progressBar.value = progressCheckpoint;
+// snake head
+
+var snakeX = blocksize * 5;
+var snakeY = blocksize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = [];
+// food 
+var foodX ;
+var foodY ;
+
+var gameOver = false;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows *blocksize;
+    board.width = cols *blocksize;
+    context = board.getContext("2d");
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    //Update();
+    setInterval(Update, 1000/10);
 }
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+function Update() {
+    if (gameOver){
+        return;
+    }
 
-let x = 0;
-let y = 0;
+    context.fillStyle = "green";
+    context.fillRect(0, 0, board.width, board.height);
 
-function update(){
-    ctx.fillRect(0, 0, 50, 50);
-    requestAnimationFrame(update);
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blocksize, blocksize);
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+    }
+
+    for (let i = snakeBody.length-1; i>0 ; i--){
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if (snakeBody.length){
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle = "lime";
+    snakeX += velocityX * blocksize;
+    snakeY += velocityY * blocksize;
+    context.fillRect(snakeX, snakeY, blocksize, blocksize);
+    for(let i = 0; i<snakeBody.length; i++){
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blocksize, blocksize);
+    }
+    //gameover condition
+    if (snakeX <0 || snakeX > cols*blocksize || snakeY < 0 || snakeY > rows*blocksize){
+        gameOver = true;
+        alert("Game Over");
+    }
+    for (let i = 0; i < snakeBody.length; i++){
+        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
 }
-update()
 
+function changeDirection(e){
+    if (e.code == "ArrowUp" && velocityY != 1){
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+function placeFood(){
+    //0-1) *cols -> (0-19.9999) ->(0-19) *25
+    foodX = Math.floor(Math.random()* cols) *blocksize;
+    foodY = Math.floor(Math.random()* rows) *blocksize;
+}
